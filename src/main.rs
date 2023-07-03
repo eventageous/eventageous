64a2e9d071875e815b3816e9420f660fd29e5f8b
Kari Wilhelm <kwdev@users.noreply.github.com>
2023-07-03 09:32:57 -0700

    Experimetning without google calendar libraries

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,42 @@
-use google_calendar::Client;
+//use google_calendar::Client;
 
 mod config;
+
+use serde::{Deserialize};
+
+#[derive(Debug, Deserialize)]
+struct EventsResponseData {
+    summary: String,
+    description: String,
+    items: Vec<Event>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Event {
+    summary: String,
+    description: Option<String>,
+    location: Option<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let config = config::Configuration::load()?;
 
-    Client
+    let endpoint = format!("https://www.googleapis.com/calendar/v3/calendars/{}/events?key={}",config.google_calendar_id, config.google_api_key);
 
-    // let google_calendar =
-    //     Client::new_from_env(String::from("token"), String::from("refresh-token")).await;
+    let response = reqwest::get(endpoint).await?;
 
-    // let events = google_calendar
-    //     .events()
-    //     .list(
-    //         "primary",
-    //         "",
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //     )
-    //     .await?;
+    if response.status().is_success() {
+        let json_body = response.text().await?;
+        let data: EventsResponseData = serde_json::from_str(&json_body)?;
+      
+        eprintln!("{data:#?}");
 
-    eprintln!("{config:#?}");
+    } else {
+        println!("Request failed with status code: {}", response.status());
+    }
+
+    //eprintln!("{config:#?}");
 
     Ok(())
 }
