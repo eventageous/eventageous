bcaedcfc17f137e6d301d5ef058b93791835ed3a
Kari Wilhelm <kwdev@users.noreply.github.com>
2023-07-03 18:10:59 -0700

    formatting

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 mod config;
 
-use serde::{Deserialize};
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct EventsResponseData {
@@ -22,16 +22,18 @@ struct Event {
 async fn main() -> Result<(), anyhow::Error> {
     let config = config::Configuration::load()?;
 
-    let endpoint = format!("https://www.googleapis.com/calendar/v3/calendars/{}/events?key={}",config.google_calendar_id, config.google_api_key);
+    let endpoint = format!(
+        "https://www.googleapis.com/calendar/v3/calendars/{}/events?key={}",
+        config.google_calendar_id, config.google_api_key
+    );
 
     let response = reqwest::get(endpoint).await?;
 
     if response.status().is_success() {
         let json_body = response.text().await?;
         let data: EventsResponseData = serde_json::from_str(&json_body)?;
-      
-        eprintln!("{data:#?}");
 
+        eprintln!("{data:#?}");
     } else {
         println!("Request failed with status code: {}", response.status());
     }
