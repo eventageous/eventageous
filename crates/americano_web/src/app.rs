bad94f4a2d4e16c2c837b3b589c78d21ea648084
Kari Wilhelm <kwdev@users.noreply.github.com>
2023-07-06 08:56:50 -0700

    Add in client/server with actix

--- crates/americano_web/src/app.rs
+++ crates/americano_web/src/app.rs
@@ -1,39 +1,71 @@
 use leptos::*;
-use std::sync::Arc;
-use unterstutzen::{Calendar, Configuration};
+use leptos_meta::*;
+use leptos_router::*;
+//use std::sync::Arc;
+//use unterstutzen::{Calendar, Configuration};
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
-    let (count, set_count) = create_signal(cx, 0);
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context(cx);
 
-    let values = create_resource(cx, || (), |_| async move { load_events().await.unwrap() });
     view! { cx,
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
 
-        <button
-            on:click=move |_| {
-                set_count.update(|n| *n += 1);
-            }
-        >
-            "Click for more shots of espresso: "
-            {move || count.get()}
-        </button>
-
-
-        <p>"Events:"</p>
-        {move || match values.read(cx) {
-                None => view! { cx, <p>"Loading events..."</p> }.into_view(cx),
-                Some(data) => view! { cx,
-                        <ul>
-                            {data.into_iter()
-                            .map(|n| view! { cx, <li>{n}</li>})
-                            .collect::<Vec<_>>()}
-                        </ul>
-                }.into_view(cx)
-        }}
+        // content for this welcome page
+        <Router>
+            <main>
+                <Routes>
+                    <Route path="" view=|cx| view! { cx, <HomePage/> }/>
+                </Routes>
+            </main>
+        </Router>
     }
+    
 }
 
-async fn load_events() -> anyhow::Result<Vec<String>> {
+/// Renders the home page of your application.
+#[component]
+fn HomePage(cx: Scope) -> impl IntoView {
+
+    let (count, set_count) = create_signal(cx, 0);
+    let values = create_local_resource(cx, || (), |_| async move { load_fake_events().await.unwrap() });
+
+    view! { cx,
+
+        <h1>"Americano ☕️"</h1>
+        
+        <button
+                on:click=move |_| {
+                    set_count.update(|n| *n += 1);
+                }
+            >
+                "Click for more shots of espresso: "
+                {move || count.get()}
+            </button>
+
+
+            <p>"Events:"</p>
+            {move || match values.read(cx) {
+                    None => view! { cx, <p>"Loading events..."</p> }.into_view(cx),
+                    Some(data) => view! { cx,
+                            <ul>
+                                {data.into_iter()
+                                .map(|n| view! { cx, <li>{n}</li>})
+                                .collect::<Vec<_>>()}
+                            </ul>
+                    }.into_view(cx)
+
+            }
+        }
+    }   
+}
+
+/*
+#[server(LoadEvents, "/api", "GetJson")]
+async fn load_events() -> Result<Vec<String>,ServerFnError> {
     web_sys::console::log_1(&"hi 0".into());
     let config = Arc::new(Configuration::load()?);
     web_sys::console::log_1(&"hi 1".into());
@@ -48,4 +80,9 @@ async fn load_events() -> anyhow::Result<Vec<String>> {
             .map(|e| format!("{e:?}"))
             .collect()
     )
+}*/
+
+#[server(LoadFakeEvents, "/api", "GetJson")]
+async fn load_fake_events() -> Result<Vec<String>,ServerFnError> {
+  Ok(vec![String::from("one"), String::from("two"), String::from("three")])
 }
--- crates/americano_web/src/lib.rs
+++ crates/americano_web/src/lib.rs
@@ -1,2 +1,21 @@
-mod app;
-pub use app::*;
+pub mod app;
+use cfg_if::cfg_if;
+
+cfg_if! {
+if #[cfg(feature = "hydrate")] {
+
+  use wasm_bindgen::prelude::wasm_bindgen;
+
+    #[wasm_bindgen]
+    pub fn hydrate() {
+      use app::*;
+      use leptos::*;
+
+      console_error_panic_hook::set_once();
+
+      leptos::mount_to_body(move |cx| {
+          view! { cx, <App/> }
+      });
+    }
+}
+}
