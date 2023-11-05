41372141acc5496fa24def1ea7c2ce6884ea644d
Kari Wilhelm <kwdev@users.noreply.github.com>
2023-11-04 17:11:38 -0700

    Add trace / logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use axum::{routing::get, Router};
 
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    tracing_subscriber::fmt().with_target(false).pretty().init();
+
+    let app = Router::new().route("/", get(handler)).layer(
+        TraceLayer::new_for_http()
+            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+    );
+
+    tracing::info!("Listening on localhost:3000");
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -11,5 +22,7 @@ async fn main() {
 }
 
 async fn handler() -> &'static str {
-    "Hello, world!"
+    let response = "Best response ever!";
+    tracing::info!("handler: {}", response);
+    response
 }
