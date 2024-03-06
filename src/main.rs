c471f7a802a33f43c8b46320d0150e93cefce430
Niko Matsakis <niko@alum.mit.edu>
2024-03-05 20:34:29 -0500

    adopt a shuttle-based backend/front setup

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,27 @@
 use axum::{routing::get, Json, Router};
-
-use tower_http::trace::{self, TraceLayer};
-use tracing::Level;
-
+use tower_http::services::ServeDir;
 use serde::Serialize;
 use std::sync::Arc;
 use unterstutzen::Calendar;
 use unterstutzen::Configuration;
 use unterstutzen::Events;
 
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .nest_service("/", ServeDir::new("frontend-ember/dist"))
+        .route("/api", get(handler));
+
+    Ok(router.into())
+}
+
 #[derive(Debug, Serialize)]
 struct Response {
     data: Events,
-}
-
-#[tokio::main]
-async fn main() {
-    tracing_subscriber::fmt().with_target(false).pretty().init();
-
-    let app = Router::new().route("/", get(handler)).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
-            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
-    );
-
-    tracing::info!("Listening on localhost:3300");
-
-    axum::Server::bind(&"0.0.0.0:3300".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
 }
 
 async fn handler() -> Json<Response> {
