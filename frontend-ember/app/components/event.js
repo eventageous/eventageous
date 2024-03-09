edc870bd978f23fcddc3198322a9a5cd995ddbd3
Kari Wilhelm <kwdev@users.noreply.github.com>
2024-03-09 09:39:10 -0500

    Link event locations that are http/https links

--- frontend-ember/app/components/event.js
+++ frontend-ember/app/components/event.js
@@ -1,6 +1,17 @@
 import Component from '@glimmer/component';
+import { htmlSafe } from '@ember/template';
 
 export default class Event extends Component {
+
+    formatLocation(location) {
+        const regex = new RegExp('^http(s)?://');
+        if (location && regex.test(location)) {
+            const link = '<a href="' + location + '">' + location + '</a>';
+            // NOTE: htmlSafe declares we trust this string as safe, it does not MAKE it safe
+            return htmlSafe(link);
+        }
+        return location;
+    }
 
     formatDescription(description) {
         // TODO: stuff
