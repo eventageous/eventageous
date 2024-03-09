c6654df69595945b02da8b54eef3ab7ac5e10d6e
Kari Wilhelm <kwdev@users.noreply.github.com>
2024-03-09 08:52:17 -0500

    Add Event helper for rendering

---
+++ frontend-ember/app/components/event.js
@@ -0,0 +1,10 @@
+import Component from '@glimmer/component';
+
+export default class Event extends Component {
+
+    formatDescription(description) {
+        // TODO: stuff
+        return description;
+    }
+
+}
